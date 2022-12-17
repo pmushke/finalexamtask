4b5b856080440cb799e4c6eefd7c4f5bf84d4e94
pmushke <106030527+pmushke@users.noreply.github.com>
2022-12-17 00:02:26 -0600

    Add files via upload

---
+++ todo-list/app/src/main/java/com/subroto/jaimes/todolist/MainActivity.java
@@ -0,0 +1,95 @@
+package com.prathap.todolist;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    DbHelper dbHelper;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setTitle("Jaimes Subroto's To-Do List");
+
+        dbHelper = new DbHelper(this);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        loadTaskList();
+    }
+
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if (adapter == null) {
+            adapter = new ArrayAdapter<String>(this, R.layout.row, R.id.task_title, taskList);
+            this.listView.setAdapter(adapter);
+        } else {
+            adapter.clear();
+            adapter.addAll(taskList);
+            adapter.notifyDataSetChanged();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        // Change menu icon color
+        Drawable icon = menu.getItem(0).getIcon();
+        icon.mutate();
+        icon.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder((this))
+                        .setTitle("Do or do not. There is no try.")
+                        .setMessage("What would you like to do?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertNewTask(task);
+                                loadTaskList();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View)view.getParent();
+        TextView taskTextView = (TextView)parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        dbHelper.deleteTask(task);
+        loadTaskList();
+    }
+}
